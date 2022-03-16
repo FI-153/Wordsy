4aa2a85f1154a0a8ff1fff7d20125cad7e5f8ec8
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-03-16 20:02:12 +0100

    Initial Commit

---
+++ Wordsy/WordsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsyApp.swift
+//  Wordsy
+//
+//  Created by Federico Imberti on 16/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WordsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
