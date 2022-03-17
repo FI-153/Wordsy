5e88a9ee110309fc7b07445cb8dda1a3e8f3a8d5
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-03-17 21:17:24 +0100

    Added SecondsView

---
+++ Wordsy/Views/SecondsView.swift
@@ -0,0 +1,89 @@
+//
+//  DataView.swift
+//  Wordsy
+//
+//  Created by Federico Imberti on 17/03/22.
+//
+
+import SwiftUI
+
+struct SecondsView: View {
+	
+	@StateObject private var vm:SecondsViewModel
+	
+	init(value:Int) {
+		_vm = .init(wrappedValue: SecondsViewModel(value: value))
+	}
+	
+    var body: some View {
+		ZStack{
+			Circle()
+				.fill(Color.primary)
+			
+			VStack{
+				Text("\(vm.getValue())")
+					.font(.system(size: 50, weight: .semibold, design: .default))
+					.foregroundColor(vm.getNumberColor())
+
+				Text("Seconds")
+					.font(.system(size: 15, weight: .light, design: .default))
+					.foregroundColor(.black)
+					.italic()
+			}
+		}
+		.frame(width: 130, height: 130)
+    }
+	
+}
+
+class SecondsViewModel: ObservableObject{
+	var value:Int
+	
+	init(value:Int) {
+		self.value = value
+	}
+	
+	func getValue() -> Int {
+		value
+	}
+	
+	func getNumberColor() -> Color {
+		if value <= 3 {
+			return .red
+		} else if value <= 10 {
+			return .orange
+		}
+		
+		return .black
+	}
+
+}
+
+struct SecondsView_Previews: PreviewProvider {
+    static var previews: some View {
+		
+		VStack {
+			HStack{
+				SecondsView(value: 60)
+				
+				SecondsView(value: 60)
+					.preferredColorScheme(.dark)
+			}
+			
+			HStack{
+				SecondsView(value: 10)
+				
+				SecondsView(value: 10)
+					.preferredColorScheme(.dark)
+			}
+			
+			HStack{
+				SecondsView(value: 3)
+				
+				SecondsView(value: 3)
+					.preferredColorScheme(.dark)
+			}
+
+		}
+    }
+}
