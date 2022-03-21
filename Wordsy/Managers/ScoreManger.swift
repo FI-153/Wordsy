93df06436f674cab3ecc44d13d7be349242eca68
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2022-03-21 21:07:30 +0100

    Created ScoreManager + tests

---
+++ Wordsy/Managers/ScoreManger.swift
@@ -0,0 +1,40 @@
+//
+//  ScoreMaanger.swift
+//  Wordsy
+//
+//  Created by Federico Imberti on 21/03/22.
+//
+
+import Foundation
+
+class ScoreManager{
+	@Published var wordsPerMinute:Float
+	@Published var charsPerMinute:Int
+	@Published var precision:Float
+	
+	static let shared = ScoreManager()
+	
+	private init(){
+		self.wordsPerMinute = 0
+		self.charsPerMinute = 0
+		self.precision = 100
+	}
+	
+	static func getShared() -> ScoreManager {
+		shared
+	}
+	
+	func increaseWordsPerMinute(){
+		wordsPerMinute += 1
+	}
+	
+	func increaseCharsPerMinute(with word: String) {
+		charsPerMinute += word.count
+	}
+	
+	func updatePrecision(for typedWords: [Word]) {
+		let correctWords = typedWords.filter { $0.wasTypedCorrecly }
+		precision = round(Float(correctWords.count)/wordsPerMinute * 100)
+	}
+	
+}
---
+++ WordsyTests/ScoreManagerTests.swift
@@ -0,0 +1,88 @@
+//
+//  ScoreManagerTests.swift
+//  WordsyTests
+//
+//  Created by Federico Imberti on 21/03/22.
+//
+
+import XCTest
+@testable import Wordsy
+
+class ScoreManagerTests: XCTestCase {
+
+	var scoreManager = ScoreManager.getShared()
+	
+	override func tearDown() {
+		scoreManager.wordsPerMinute = 0
+		scoreManager.charsPerMinute = 0
+		scoreManager.precision = 100
+	}
+	
+	func test_ScoreManager_increaseWordsPerMinute_wordsPerMinuteIsIncreasedByOne(){
+		//Given
+		let expectedWordsPerMinute = scoreManager.wordsPerMinute + 1
+		//When
+		scoreManager.increaseWordsPerMinute()
+		//Then
+		XCTAssertEqual(expectedWordsPerMinute, scoreManager.wordsPerMinute)
+	}
+	
+	func test_ScoreManager_increaseCharsPerMinute_correctlyIncreased(){
+		//Given
+		let word = "Hello"
+		let expectedLength = 5
+		//When
+		scoreManager.increaseCharsPerMinute(with: word)
+		//Then
+		XCTAssertEqual(expectedLength, scoreManager.charsPerMinute)
+	}
+	
+	func test_ScoreManager_updatePrecision_precisionIs100Then50() {
+		//Given
+		var word = Word("abc")
+		word.wasTypedCorrecly = true
+		var words:[Word] = [word]
+		//When
+		scoreManager.increaseWordsPerMinute()
+		scoreManager.updatePrecision(for: words)
+		//Then
+		XCTAssertEqual(Float(100), scoreManager.precision)
+		
+		//Given
+		word.wasTypedCorrecly = false
+		words.append(word)
+		//When
+		scoreManager.increaseWordsPerMinute()
+		scoreManager.updatePrecision(for: words)
+		//Then
+		XCTAssertEqual(Float(50), scoreManager.precision)
+	}
+	
+	func test_ScoreManager_updatePrecision_precisionIs33(){
+		//Given
+		var word = Word("abc")
+		var words:[Word] = []
+		
+		//When
+		word.wasTypedCorrecly = true
+		words.append(word)
+		scoreManager.increaseWordsPerMinute()
+		scoreManager.updatePrecision(for: words)
+		
+		//When
+		word.wasTypedCorrecly = false
+		words.append(word)
+		scoreManager.increaseWordsPerMinute()
+		scoreManager.updatePrecision(for: words)
+
+		//When
+		word.wasTypedCorrecly = false
+		words.append(word)
+		scoreManager.increaseWordsPerMinute()
+		scoreManager.updatePrecision(for: words)
+		
+		//Then
+		XCTAssertEqual(Float(33), scoreManager.precision)
+	}
+
+}
